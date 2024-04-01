ec4df4154d5ea437d7d264d8a02ed62186c158d6
Araon <soumik.ghosh1998@gmail.com>
2024-04-02 00:38:06 +0530

    made some changes to storeImages

--- app/api/storeImages/route.ts
+++ app/api/storeImages/route.ts
@@ -1,10 +1,9 @@
 // pages/api/unsplash-images.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import { createApi } from 'unsplash-js';
+import { createApi } from "unsplash-js";
 import { PrismaClient } from "@prisma/client";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -13,18 +12,19 @@ const prisma = new PrismaClient();
 // @ts-ignore
 const unsplash = createApi({
   accessKey: process.env.UNSPLASH_ACCESS_KEY,
-  headers: {
-    'Authorization': process.env.UNSPLASH_AUTHORIZATION
-  }
 });
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   try {
-    const username = 'ara0n'; // Replace with your Unsplash username
-    const responseData = await unsplash.users.getPhotos({ username, perPage: 100, stats: true});
+    const username = "ara0n"; // Replace with your Unsplash username
+    const responseData = await unsplash.users.getPhotos({
+      username,
+      perPage: 100,
+      stats: true,
+    });
 
     const data = responseData.response;
-    console.log(data)
+    console.log(data);
 
     if (!data?.results) {
       return new Response("No results found", { status: 404 });
@@ -33,12 +33,12 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
     const photos = data?.results.map((photo: any) => ({
       photo_id: photo.id,
       photo_url: photo.urls.regular,
-      alt_text: photo.alt_description || '',
+      alt_text: photo.alt_description || "",
       is_published: true,
       color: photo.color,
       views: photo.statistics?.views?.total || 0,
       height: photo.height,
-      width: photo.width
+      width: photo.width,
     }));
 
     const savedPhotos = await prisma.photos.createMany({
