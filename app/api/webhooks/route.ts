ce6992e20a49405db203dc1db2108e48eba1dbd1
Araon <soumik.ghosh1998@gmail.com>
2024-04-07 15:17:19 +0530

    body parsed - insert only and ip address lookup

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
-const CHAT_ID = process.env.CHAT_ID
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const { postId, content, author } = body;
 
-    console.log("Got data from database: ", body);
+    if (body.type !== "INSERT") {
+      return NextResponse.json(
+        { error: "Only INSERT events are supported" },
+        { status: 400 },
+      );
+    }
+
+    const { author, postId, content, ip_identity } = body?.record;
 
     if (!content || !author) {
       return NextResponse.json(
@@ -18,13 +24,16 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const message = `New comment added to your blog!\n\nPost Link: https://araon.space/blog/${postId}\nName: ${author}\nComment: ${content}`;
+    const message = `New comment added to your blog!\n\nPost Link: https://araon.space/blog/${postId}\n\nName: ${author}\nComment: ${content}\n\nIP: https://whatismyipaddress.com/ip/${ip_identity}`;
 
     // Send the message to Telegram using the bot token and chat ID
-    await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-      chat_id: CHAT_ID,
-      text: message,
-    });
+    await axios.post(
+      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        chat_id: CHAT_ID,
+        text: message,
+      },
+    );
 
     return NextResponse.json({ comment: "Comment added" }, { status: 201 });
   } catch (error) {
