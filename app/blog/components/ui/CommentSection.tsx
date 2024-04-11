b869a4e88626732d107df1ca9f7708ac61bad9de
Araon <soumik.ghosh1998@gmail.com>
2024-04-11 23:18:20 +0530

    fixed bug - disable comments

--- app/blog/components/ui/CommentSection.tsx
+++ app/blog/components/ui/CommentSection.tsx
@@ -2,62 +2,60 @@
 
 import { FormEventHandler, useCallback, useState } from "react";
 import DOMPurify from "dompurify";
-
 import Halo from "@/components/ui/Halo";
 
 export default function CommentSection({ postId }: { postId: string }) {
   const [isSuccess, setIsSuccess] = useState<boolean | undefined>();
-  const onSubmit: FormEventHandler = useCallback(
-    async (event) => {
-      event.preventDefault();
+  const [isCommentSubmitted, setIsCommentSubmitted] = useState(false);
 
-      const target = event.target as HTMLFormElement;
-      const data = new FormData(target);
+  const onSubmit: FormEventHandler = useCallback(async (event) => {
+    event.preventDefault();
 
-      const author = data.get("name")?.toString() ?? "";
-      const content = data.get("comment")?.toString() ?? "";
+    const target = event.target as HTMLFormElement;
+    const data = new FormData(target);
 
+    const author = data.get("name")?.toString() ?? "";
+    const content = data.get("comment")?.toString() ?? "";
 
-      const sanitizedAuthor = DOMPurify.sanitize(author);
-      const sanitizedontent = DOMPurify.sanitize(content);
+    const sanitizedAuthor = DOMPurify.sanitize(author);
+    const sanitizedContent = DOMPurify.sanitize(content);
 
-      const trimAuthor = sanitizedAuthor.trim();
-      const trimContent = sanitizedontent.trim();
+    const trimAuthor = sanitizedAuthor.trim();
+    const trimContent = sanitizedContent.trim();
 
-      console.log("sending data to backend: ", trimContent, trimAuthor);
+    console.log("sending data to backend: ", trimContent, trimAuthor);
 
-      const body = JSON.stringify({
-        postId,
-        author: trimAuthor,
-        content: trimContent,
+    const body = JSON.stringify({
+      postId,
+      author: trimAuthor,
+      content: trimContent,
+    });
+
+    const headers = new Headers({
+      "Content-Type": "application/json; charset=utf-8",
+    });
+
+    console.log(body);
+
+    try {
+      const response = await fetch(`/api/prisma/postComments`, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        headers,
+        body,
       });
 
-      const headers = new Headers({
-        "Content-Type": "application/json; charset=utf-8",
-      });
-
-      console.log(body);
-
-      try {
-        const response = await fetch(`/api/prisma/postComments`, {
-          method: "POST",
-          mode: "cors",
-          cache: "no-cache",
-          headers,
-          body,
-        });
-
-        if (response.status === 201) {
-          setIsSuccess(true);
-        } else {
-          setIsSuccess(false);
-        }
-      } catch {
+      if (response.status === 201) {
+        setIsSuccess(true);
+        setIsCommentSubmitted(true);
+      } else {
         setIsSuccess(false);
       }
-    },
-    [postId],
-  );
+    } catch {
+      setIsSuccess(false);
+    }
+  }, [postId]);
 
   const message =
     isSuccess === false ? (
@@ -77,48 +75,50 @@ export default function CommentSection({ postId }: { postId: string }) {
   return (
     <div className="bg-dark flex w-full flex-col rounded-lg p-6">
       {message}
-      <form onSubmit={onSubmit} className="flex flex-col space-y-4">
-        <div className="flex flex-col">
-          <label
-            htmlFor="name"
-            className="text-l text-dark mb-4 font-medium dark:text-white"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-zinc-950 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Name"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label
-            htmlFor="comment"
-            className="text-l text-dark mb-2 font-medium dark:text-white"
-          >
-            Comment
-          </label>
-          <textarea
-            name="comment"
-            id="comment"
-            rows={2}
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-zinc-950 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Type a comment"
-          />
-        </div>
-        <Halo strength={0} className="rounded-lg">
-          <button
-            type="submit"
-            className="group relative mb-2 me-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-red-100 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 dark:focus:ring-red-400"
-          >
-            <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-zinc-950">
-              Post Comment
-            </span>
-          </button>
-        </Halo>
-      </form>
+      {!isCommentSubmitted && (
+        <form onSubmit={onSubmit} className="flex flex-col space-y-4">
+          <div className="flex flex-col">
+            <label
+              htmlFor="name"
+              className="text-l text-dark mb-4 font-medium dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-zinc-950 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="Name"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="comment"
+              className="text-l text-dark mb-2 font-medium dark:text-white"
+            >
+              Comment
+            </label>
+            <textarea
+              name="comment"
+              id="comment"
+              rows={2}
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-zinc-950 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="Type a comment"
+            />
+          </div>
+          <Halo strength={0} className="rounded-lg">
+            <button
+              type="submit"
+              className="group relative mb-2 me-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-red-100 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 dark:focus:ring-red-400"
+            >
+              <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-zinc-950">
+                Post Comment
+              </span>
+            </button>
+          </Halo>
+        </form>
+      )}
     </div>
   );
 }
